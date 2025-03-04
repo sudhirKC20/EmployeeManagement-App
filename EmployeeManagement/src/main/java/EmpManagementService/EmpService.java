0a5f62135a6b3f362ac95fe58907b4379ed77668
sudhi <sudhi@Captain>
2025-03-04 23:04:43 +0530

    commit

--- EmployeeManagement/src/main/java/EmpManagementService/EmpService.java
+++ EmployeeManagement/src/main/java/EmpManagementService/EmpService.java
@@ -1,5 +1,35 @@
 package EmpManagementService;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.*;
+
 public class EmpService {
+	private static Connection con;
+	private static String url = "jdbc:postgresql://localhost:5432/employeemanagement?user=postgres&password=123";
+	private static Scanner sc = new Scanner(System.in);
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+			DriverManager.getConnection(url);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public save() {
+		int res = 0;
+		System.out.println("Enter the employee id: ");
+		int id = sc.nextInt();
+		System.out.println("Enter the employee name: ");
+		String name = sc.next();
+		System.out.println("Entetr the employee age: ");
+		int age = sc.nextInt();
+		
+	}
 
 }
