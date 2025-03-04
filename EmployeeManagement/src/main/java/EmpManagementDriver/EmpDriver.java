3f4b73f816a70b183a0faa4ebb879a1ddf7d5cf5
sudhi <sudhi@Captain>
2025-03-04 22:05:09 +0530

    commit

--- EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
+++ EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
@@ -1,5 +1,33 @@
 package EmpManagementDriver;
+import java.util.*;
+import EmpManagementService.EmpService;
 
 public class EmpDriver {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Management Application");
+		System.out.println("1.Register Employee ");
+		System.out.println("2.Update Employee  ");
+		System.out.println("3.Delete Employee ");
+		System.out.println("4.Fetch all Employee Data ");
+		System.out.println("5.Close the Connection ");
+		
+		Scanner sc = new Scanner(System.in);
+		EmpService e1 = new EmpService();
+		
+		System.out.println("Choose any: ");
+		int choice = sc.nextInt();
+		
+		switch (choice) {
+		case 1:
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+	}
 
 }
