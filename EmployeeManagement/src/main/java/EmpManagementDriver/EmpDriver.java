cb48eb82ede3694666cb0bc86b2a713b59c9538a
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-03-06 19:31:03 +0530

    printing the fetched the data from the table

--- EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
+++ EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
@@ -1,45 +1,71 @@
 package EmpManagementDriver;
+
 import java.util.*;
 import EmpManagementService.EmpService;
+import EmpManagementService.Employee;
 
 public class EmpDriver {
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Employee Management Application");
-		System.out.println("1.Register Employee ");
-		System.out.println("2.Update Employee  ");
-		System.out.println("3.Delete Employee ");
-		System.out.println("4.Fetch all Employee Data ");
-		System.out.println("5.Close the Connection ");
-		
-		Scanner sc = new Scanner(System.in);
-		EmpService e1 = new EmpService();
-		
-		System.out.println("Choose any: ");
-		int choice = sc.nextInt();
-		
-		switch (choice) {
-		case 1:
-			int res = e1.save();
-			if(res!=0) System.out.println("Data saved sucessfully.");
-			else System.out.println("Data not saved.");
-			break;
-			
-		case 2:
-			int res1 = e1.update();
-			if(res1!=0) System.out.println("Data updated sucessfully.");
-			else System.out.println("Data not updated");
-			break;
-			
-		case 3:
-			int res2 = e1.delete();
-			if(res2!=0) System.out.println("Data deleted sucessfully.");
-			else System.out.println("Data not deleted.");
 
-		default:
-			break;
-		}
+		System.out.println("Welcome to Employee Management Application");
+		boolean flag = true;
+		while (flag) {
+			System.out.println("========================");
+			System.out.println("1.Register Employee ");
+			System.out.println("2.Update Employee  ");
+			System.out.println("3.Delete Employee ");
+			System.out.println("4.Fetch all Employee Data ");
+			System.out.println("5.Close the Connection ");
+
+			Scanner sc = new Scanner(System.in);
+			EmpService e1 = new EmpService();
+
+			System.out.println("Choose any: ");
+			int choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				int res = e1.save();
+				if (res != 0)
+					System.out.println("Data saved sucessfully.");
+				else
+					System.out.println("Data not saved.");
+				break;
+
+			case 2:
+				int res1 = e1.update();
+				if (res1 != 0)
+					System.out.println("Data updated sucessfully.");
+				else
+					System.out.println("Data not updated");
+				break;
+
+			case 3:
+				int res2 = e1.delete();
+				if (res2 != 0)
+					System.out.println("Data deleted sucessfully.");
+				else
+					System.out.println("Data not deleted.");
+				break;
+
+			case 4:
+				List<Employee> emp = e1.fetchAll();
+				if(emp!=null) {
+					for( Employee e :emp) {
+						System.out.println(e.id+" "+e.name+" "+e.age+" "+e.salary);
+					}
+				}
+
+				break;	
 				
+
+				
+
+			default:
+				break;
+			}
+		}
+
 	}
 
 }
