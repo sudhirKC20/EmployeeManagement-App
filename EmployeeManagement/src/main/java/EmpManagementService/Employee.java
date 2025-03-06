b8fc49c593f50e0e6acaf1003dccf82021b228b3
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-03-06 19:25:00 +0530

    Employee class is created

---
+++ EmployeeManagement/src/main/java/EmpManagementService/Employee.java
@@ -0,0 +1,16 @@
+package EmpManagementService;
+
+public class Employee {
+	public int id;
+	public String name;
+	public int age;
+	public int salary;
+	public Employee(int id, String name, int age, int salary) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+
+}
