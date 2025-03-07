fc6e51a48d456508f8d907ebbf0940934c12f392
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-03-07 20:43:05 +0530

    commit

--- EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
+++ EmployeeManagement/src/main/java/EmpManagementDriver/EmpDriver.java
@@ -9,18 +9,18 @@ public class EmpDriver {
 
 		System.out.println("Welcome to Employee Management Application");
 		boolean flag = true;
+
+		System.out.println("========================");
+		System.out.println("1.Register Employee ");
+		System.out.println("2.Update Employee  ");
+		System.out.println("3.Delete Employee ");
+		System.out.println("4.Fetch all Employee Data ");
+		System.out.println("5.Close the Connection \n");
+
+		Scanner sc = new Scanner(System.in);
+		EmpService e1 = new EmpService();
 		while (flag) {
-			System.out.println("========================");
-			System.out.println("1.Register Employee ");
-			System.out.println("2.Update Employee  ");
-			System.out.println("3.Delete Employee ");
-			System.out.println("4.Fetch all Employee Data ");
-			System.out.println("5.Close the Connection ");
-
-			Scanner sc = new Scanner(System.in);
-			EmpService e1 = new EmpService();
-
-			System.out.println("Choose any: ");
+			System.out.println("Select any option:");
 			int choice = sc.nextInt();
 
 			switch (choice) {
@@ -50,22 +50,22 @@ public class EmpDriver {
 
 			case 4:
 				List<Employee> emp = e1.fetchAll();
-				if(emp!=null) {
-					for( Employee e :emp) {
-						System.out.println(e.id+" "+e.name+" "+e.age+" "+e.salary);
+				if (emp != null) {
+					for (Employee e : emp) {
+						System.out.println(e.id + " " + e.name + " " + e.age + " " + e.salary);
 					}
 				}
 
-				break;	
-				
+				break;
+
 			case 5:
-				boolean flag1 =  e1.close();
-				if(flag1) {
+				boolean flag1 = e1.close();
+				if (flag1) {
 					System.out.println("Application closed");
 					flag = false;
 				}
 				break;
-				
+
 			default:
 				break;
 			}
